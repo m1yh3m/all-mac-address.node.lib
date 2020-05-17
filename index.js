fcf5f7a76fa0333990b8295795a5ff22829c2781
m1yh3m <m1yh3m@protonmail.com>
2020-05-18 00:09:43 +0530

    V1.0.1 - BUG FIX

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function allMacAddressCommand() {
   switch(process.platform) {
     case 'darwin':
       return `ifconfig | grep -v "\t" | awk -F':' '{print $1}' | xargs -n1 networksetup -getmacaddress | grep -v "Error"`
-    case 'linux'
+    case 'linux':
       return `ifconfig -a | grep "^[a-zA-Z0-9]" | awk -F':' '{print cat $1}' | while read -r line; do  cat /sys/class/net/$line/address; done`
     default:
       throw new Error(`Platform ${process.platform} is not supported! Please create an issue in github.`)
