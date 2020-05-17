0183482d8d22f9f90c975f7535a010650e2f4921
m1yh3m <m1yh3m@protonmail.com>
2020-05-17 23:56:24 +0530

    V1

---
+++ README.md
@@ -0,0 +1,11 @@
+# All Mac Address Node Lib
+
+It's just a node library. This function returns a string or throws an error if platform is not supported.
+
+```bash
+
+var ama_cmd = require('@m1yh3m/all-mac-address.node.lib')
+
+# Then you can exec it on linux or mac systems.
+```
+
---
+++ index.js
@@ -0,0 +1,15 @@
+function allMacAddressCommand() {
+  switch(process.platform) {
+    case 'darwin':
+      return `ifconfig | grep -v "\t" | awk -F':' '{print $1}' | xargs -n1 networksetup -getmacaddress | grep -v "Error"`
+    case 'linux'
+      return `ifconfig -a | grep "^[a-zA-Z0-9]" | awk -F':' '{print cat $1}' | while read -r line; do  cat /sys/class/net/$line/address; done`
+    default:
+      throw new Error(`Platform ${process.platform} is not supported! Please create an issue in github.`)
+  }
+}
+
+if(module){
+  module.exports = allMacAddressCommand
+}
+
